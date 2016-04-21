e5b63426dae34eb466e3dd78563fe6ca2d7f5218
tommy-u <tommyu@bu.edu>
2016-04-21 17:36:20 -0400

    Building GPU support.

--- recVsForExpt/trainNetGPU.lua
+++ recVsForExpt/trainNetGPU.lua
@@ -1,27 +1,34 @@
 require 'nn';
+require 'cutorch';
 --The net.
 mlp = torch.load("the_mlp_net");
 processPath = "../process/";
 --The datasets.
 trainTable = torch.load( processPath.."outputTrainTable" );
 
+mlp_c = mlp:cuda();
+--trainTable_c = trainTable:cuda()  --This fails because trainTable is a table, not a tensor. 
+--[[
+input = torch.Tensor(1,2800,1);
+for i= 1,#trainTable do
+   input[i][i][1] = trainTable[i][1]
+   input[i][i][2] = trainTable[i][2]
+end
+--]]
+
+
 --Need to provide this function for training. 
 function trainTable:size() return #trainTable end
 
 --This is the loss function, MSE.
-criterion = nn.ClassNLLCriterion();
+criterion = nn.ClassNLLCriterion():cuda();
 --This is the training technique, SGD.
-trainer = nn.StochasticGradient(mlp, criterion);
+trainer = nn.StochasticGradient(mlp_c, criterion);
 --Default learning rate is 0.01
 --trainer.learningRate = 0.01
 --trainer.maxIteration = 100
 
 --Train, does 25 rounds by default
+trainer:train(trainTable_c);
 
-
-print(trainTable);
-
-trainer:train(trainTable);
---trainer:train(3d_ten);
-
-torch.save("trained_mlp_net", mlp)
+torch.save("trained_mlp_net_c", mlp_c)
