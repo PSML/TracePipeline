90b473e1625493e6ca8eb39bcfe51d08dcafbccb
tommy-u <tommyu@bu.edu>
2016-05-05 01:32:05 -0400

    queue stuff mostly

--- cincExpt/CPU/runmyscript.sh
+++ cincExpt/CPU/runmyscript.sh
@@ -4,6 +4,8 @@
 pid=`qsub ./myscript.sh | cut -d ' ' -f 3`
 echo "submitted job to queue"
 echo "The pid is $pid and output file is myscript.sh.o$pid" 
+qstat -u tommyu
+touch myscript.sh.o$pid
 tail -f myscript.sh.o$pid
 
 
