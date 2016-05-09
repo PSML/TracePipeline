17aaeeb30189675e3a218b5054f361ea076c2197
tommy-u <tommyu@bu.edu>
2016-05-09 19:35:06 -0400

    Parallelized data load. 2.5x faster for ./genExps.sh.

--- data/genExps.sh
+++ data/genExps.sh
@@ -5,41 +5,73 @@ cd exp1CompressedData
 tar -vxjf exp1ForCountBmps.tar.bz2
 tar -vxjf exp1RecCountBmps.tar.bz2
 
-mogrify -format png exp1ForCountBmps/*
-mogrify -format png exp1RecCountBmps/*
-
 mkdir ../exp1ForCountPngs
 mkdir ../exp1RecCountPngs
 
-mv exp1ForCountBmps/*.png ../exp1ForCountPngs
-mv exp1RecCountBmps/*.png ../exp1RecCountPngs
+count=0
+MAXJOBS=8
+for i in $(ls exp1ForCountBmps)
+do 
+    (( $count % $MAXJOBS )) ||  wait; #Wait for all to return
+    (
+    mogrify -format png exp1ForCountBmps/"$i";
+    mv exp1ForCountBmps/"${i%bmp}"png ../exp1ForCountPngs;
+    ) &
+    count=$((count+1))
+done
 
+count=0
+for i in $(ls exp1RecCountBmps)
+do
+    (( $count % $MAXJOBS )) ||  wait; #Wait for all to return
+    (
+    mogrify -format png exp1RecCountBmps/"$i"
+    mv exp1RecCountBmps/"${i%bmp}"png ../exp1RecCountPngs
+    ) &
+    count=$((count+1))
+done
+wait
 rm -rf exp1ForCountBmps
 rm -rf exp1RecCountBmps
+
 cd ..
 
 #=====================
 echo prep exp 2 data
 cd exp2CompressedData
 tar -vxjf exp2IncBmps.tar.bz2
-
-mogrify -format png exp2IncBmps/*
-
 mkdir ../exp2IncPngs
-mv exp2IncBmps/*.png ../exp2IncPngs/
+
+count=0
+for i in $(ls exp2IncBmps)
+do
+    (( $count % $MAXJOBS )) ||  wait; #Wait for all to return
+    (
+    mogrify -format png exp2IncBmps/"$i"
+    mv exp2IncBmps/"${i%bmp}"png ../exp2IncPngs/
+    ) &
+    count=$((count+1))    
+done
+wait
 rm -rf exp2IncBmps
 cd ..
 
 #=====================
 cd exp3CompressedData
-
 tar -vxjf exp3IncBmps.tar.bz2
-
-mogrify -format png exp3IncBmps/*
-
 mkdir ../exp3IncPngs
-mv exp3IncBmps/*.png ../exp3IncPngs/
 
+count=0
+for i in $(ls exp3IncBmps)
+do
+    (( $count % $MAXJOBS )) ||  wait; #Wait for all to return
+    (
+    mogrify -format png exp3IncBmps/"$i"
+    mv exp3IncBmps/"${i%bmp}"png ../exp3IncPngs/
+    ) &
+    count=$((count+1))
+done
+wait
 rm -rf exp3IncBmps
 cd ..
 
