767e0a2e6b0679033c53dba61e6fcf92f1540c1e
Tommy Unger <tunger10@gmail.com>
2016-06-28 12:24:01 -0400

    Built exp1.

--- experiments/e1_forvsrec/run.lua
+++ experiments/e1_forvsrec/run.lua
@@ -1,4 +1,7 @@
+local trtotable = require '../../process/trtotable'
+local sm = require '../../models/simpleModel.lua'
 require 'lfs'
+
 math.randomseed( os.time() )
 
 local sf = string.format
@@ -14,7 +17,7 @@ for i=1,2 do
    ex("make START=" .. start .. " MAX=" .. start+10 )
 end
 
---Generate traces, something fishy here.
+--Generate traces, something fishy here. aborts 
 ex("mkdir traces")
 for file in lfs.dir("images") do
    ex("../../datasources/6502/6502 -t sv -o traces/" .. file .. ".trc images/" .. file .." /dev/null 2> tmp")
@@ -22,10 +25,43 @@ end
 
 --Generate bmps
 --broken no google code page
+--Use stubs for now.
 
---Generate torch tensors from bmps
+local rectr, recte = trtotable.gentrtetab("../../datasources/stub_data/exp1RecCountPngs/", 0.8)
+local fortr, forte = trtotable.gentrtetab("../../datasources/stub_data/exp1ForCountPngs/", .8)
 
---Run through model 
+
+--Give recursive label 1
+trtotable.tablelabel(rectr, 1)
+trtotable.tablelabel(recte, 1)
+--Give for loop label 2
+trtotable.tablelabel(fortr, 2)
+trtotable.tablelabel(forte, 2)
+
+--print(#rectr)
+--print(#recte)
+--print(#fortr)
+--print(#forte)
+
+
+--Combine 4 tables into 2.
+local trainSet = trtotable.cattables(rectr, fortr)
+local testSet  = trtotable.cattables(recte, forte)
+
+--print(#rectr + #fortr)
+--print(#recte + #forte)
+
+--print(#trainSet)
+--print(#testSet) 
+
+--Generate model
+local mod = sm.buildModel(trainSet)
+
+sm.train(mod, trainSet)
+print(#testSet)
+sm.test(mod, testSet)
+
+
 
 --[[
    print(file)
